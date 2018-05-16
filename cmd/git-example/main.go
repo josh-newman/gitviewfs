9d75c19b7881ed9220ab798da95ad892b87a9de4
Josh Newman <josh@josh-newman.com>
2018-05-15 23:32:29 -0700

    cmd/git-example: Add file previews.

--- cmd/git-example/main.go
+++ cmd/git-example/main.go
@@ -12,6 +12,8 @@ import (
 	"gopkg.in/src-d/go-git.v4/plumbing/filemode"
 	"strings"
 	"path"
+	"bufio"
+	"unicode"
 )
 
 func main() {
@@ -68,27 +70,75 @@ func main() {
 				dirDepth = len(strings.Split(path.Clean(dirPath), "/"))
 			}
 			dirPadding := strings.Repeat("|  ", dirDepth)
-			fmt.Printf("  %s|- %s\n", dirPadding, getDisplayName(entry))
+
+			fmt.Printf("  %s|- %s\n", dirPadding, getDisplayName(&entry, branchTree))
 		}
 	}
 }
 
-func getDisplayName(entry object.TreeEntry) string {
+func getDisplayName(entry *object.TreeEntry, tree *object.Tree) string {
 	switch entry.Mode {
 	case filemode.Empty:
 		return fmt.Sprintf("%s [0]", entry.Name)
 	case filemode.Dir:
 		return fmt.Sprintf("%s/", entry.Name)
 	case filemode.Regular, filemode.Deprecated:
-		return fmt.Sprintf("%s [%s]", entry.Name, entry.Hash.String())
+		file, err := tree.TreeEntryFile(entry)
+		if err != nil {
+			log.Fatal(errors.Wrapf(err, "get file failed for entry %s", entry.Hash))
+		}
+		var preview string
+		if isBinary, err := file.IsBinary(); err != nil {
+			log.Fatalf("Unable to test if file %s is binary: %s", file.Hash, err)
+		} else if isBinary {
+			preview = "(binary)"
+		} else {
+			reader, err := file.Reader()
+			defer reader.Close()
+			if err != nil {
+				log.Fatalf("Unable to read file %s: %s", file.Hash, err)
+			}
+			fileHead, err := readBoundedString(bufio.NewReader(reader), 40)
+			if err != nil {
+				log.Fatalf("Unable to read head of file %s: %s", file.Hash, err)
+			}
+			preview = fmt.Sprintf(`"%s"`, fileHead)
+		}
+		return fmt.Sprintf("%s [%s] %s", file.Name, file.Hash.String()[:8], preview)
 	case filemode.Executable:
-		return fmt.Sprintf("%s [* %s]", entry.Name, entry.Hash.String())
+		return fmt.Sprintf("%s [* %s]", entry.Name, entry.Hash.String()[:8])
 	case filemode.Symlink:
 		return fmt.Sprintf("%s [->]", entry.Name)
 	case filemode.Submodule:
 		return fmt.Sprintf("%s [@]", entry.Name)
 	}
 
-	log.Fatalf("Unrecognized mode %s for tree entry: %s", entry.Mode, entry.Hash)
+	log.Fatalf("Unrecognized mode %s for entry: %s", entry.Mode, entry.Hash)
 	panic("unreachable")
+}
+
+func readBoundedString(reader *bufio.Reader, maxLen int) (string, error) {
+	var runes []rune
+	for len(runes) < maxLen + 1 {
+		r, _, err := reader.ReadRune()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return "", err
+		}
+		if !unicode.IsPrint(r) {
+			if unicode.IsSpace(r) {
+				r = ' '
+			} else {
+				r = unicode.ReplacementChar
+			}
+		}
+		runes = append(runes, r)
+	}
+	if len(runes) == maxLen + 1 {
+		// The file is longer than maxLen, so we need to abridge.
+		runes = runes[:maxLen]
+		runes[maxLen - 1] = '…'
+	}
+	return string(runes), nil
 }
