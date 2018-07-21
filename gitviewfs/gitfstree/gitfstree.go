ca1f734cc1398698bb50781adf0d60dac340da01
Josh Newman <josh@josh-newman.com>
2018-07-21 09:11:43 -0700

    List all refs, not just branches.

--- gitviewfs/gitfstree/gitfstree.go
+++ gitviewfs/gitfstree/gitfstree.go
@@ -14,71 +14,74 @@ import (
 )
 
 func New(repo *git.Repository) (fstree.Node, error) {
-	branchRefs, err := repo.Branches()
-	defer branchRefs.Close()
+	refs, err := repo.References()
+	defer refs.Close()
 	if err != nil {
-		return nil, errors.Wrap(err, "list branches failed")
+		return nil, errors.Wrap(err, "list references failed")
 	}
 
-	node := branchesNode{repo: repo}
-	err = branchRefs.ForEach(func(branchRef *plumbing.Reference) error {
-		nameParts := strings.Split(string(branchRef.Name()), "/")
-		node.entries = append(node.entries, branchesNodeEntry{nameParts: nameParts, branchRef: branchRef})
+	node := referencesNode{repo: repo}
+	err = refs.ForEach(func(ref *plumbing.Reference) error {
+		if ref.Name() == plumbing.HEAD {
+			return nil
+		}
+		nameParts := strings.Split(string(ref.Name()), "/")
+		node.entries = append(node.entries, referencesNodeEntry{nameParts: nameParts, ref: ref})
 		return nil
 	})
 	if err != nil {
-		return nil, fserror.Unexpected(errors.Wrap(err, "next branch failed"))
+		return nil, fserror.Unexpected(errors.Wrap(err, "processing references failed"))
 	}
 
 	return &node, nil
 }
 
-type branchesNodeEntry struct {
+type referencesNodeEntry struct {
 	nameParts []string
-	branchRef *plumbing.Reference
+	ref       *plumbing.Reference
 }
 
-type branchesNode struct {
+type referencesNode struct {
 	repo    *git.Repository
-	entries []branchesNodeEntry
+	entries []referencesNodeEntry
 }
 
-func (n *branchesNode) Children() (map[string]fstree.Node, *fserror.Error) {
+func (n *referencesNode) Children() (map[string]fstree.Node, *fserror.Error) {
 	children := map[string]fstree.Node{}
 	for _, entry := range n.entries {
 		switch len(entry.nameParts) {
 		case 0:
-			return nil, fserror.Unexpected(errors.Errorf("unexpected branch name: %s", entry.branchRef.Name()))
+			return nil, fserror.Unexpected(errors.Errorf("unexpected ref name: %s", entry.ref.Name()))
 
 		case 1:
-			branchCommit, err := n.repo.CommitObject(entry.branchRef.Hash())
+			refCommit, err := n.repo.CommitObject(entry.ref.Hash())
 			if err == plumbing.ErrObjectNotFound {
-				return nil, fserror.Unexpected(errors.Errorf("Branch %s points to invalid or non-commit ref %s", entry.branchRef.Name(), entry.branchRef.Hash()))
+				return nil, fserror.Unexpected(errors.Errorf("Ref name %s points to invalid or non-commit ref %s", entry.ref.Name(), entry.ref.Hash()))
 			} else if err != nil {
-				return nil, fserror.Unexpected(errors.Wrap(err, "find branch commit failed"))
+				return nil, fserror.Unexpected(errors.Wrap(err, "find ref commit failed"))
 			}
 
-			branchTree, err := branchCommit.Tree()
+			refTree, err := refCommit.Tree()
 			if err != nil {
-				return nil, fserror.Unexpected(errors.Wrap(err, "find branch tree failed"))
+				return nil, fserror.Unexpected(errors.Wrap(err, "find ref tree failed"))
 			}
 
-			children[entry.nameParts[0]] = &treeNode{repo: n.repo, tree: branchTree}
+			children[entry.nameParts[0]] = &treeNode{repo: n.repo, tree: refTree}
 
 		default:
-			var child *branchesNode
+			var child *referencesNode
 			if existingNode, ok := children[entry.nameParts[0]]; ok {
-				if existingBranchesNode, ok := existingNode.(*branchesNode); ok {
+				if existingBranchesNode, ok := existingNode.(*referencesNode); ok {
 					child = existingBranchesNode
 				} else {
-					return nil, fserror.Unexpected(errors.Errorf("conflicting parent/child branch name: %v", entry.branchRef.Name()))
+					return nil, fserror.Unexpected(errors.Errorf("conflicting parent/child branch name: %v", entry.ref.Name()))
 				}
 			} else {
-				child = &branchesNode{repo: n.repo}
+				child = &referencesNode{repo: n.repo}
 				children[entry.nameParts[0]] = child
 			}
 
-			child.entries = append(child.entries, branchesNodeEntry{nameParts: entry.nameParts[1:], branchRef: entry.branchRef})
+			child.entries = append(child.entries, referencesNodeEntry{nameParts: entry.nameParts[1:], ref: entry.ref})
 		}
 	}
 	return children, nil
